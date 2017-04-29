0b523a1e579171110e847b1ccc2b08c5237115be
shigemk2 <i.am.shige@gmail.com>
2017-04-29 21:07:05 +0900

    filterlink

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import { setVisibilityFilter } from '../actions'
 import Link from '../components/Link'
 
+// タスクのフィルタリングのロジック実装
 const mapStateToProps = (state, ownProps) => ({
   active: ownProps.filter === state.visibilityFilter
 })
