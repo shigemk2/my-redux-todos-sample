62a7817ec467ea772ddc402804390da7bf292b43
shigemk2 <i.am.shige@gmail.com>
2017-04-29 15:05:21 +0900

    コロン

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,18 @@
 // Actionsはアプリからstoreにデータを送る情報のペイロード
-let nextTodoId = 0
+let nextTodoId = 0;
 export const addTodo = (text) => ({
   // 必ずtypeが必要
   type: 'ADD_TODO',
   id: nextTodoId++,
   text
-})
+});
 
 export const setVisibilityFilter = (filter) => ({
   type: 'SET_VISIBILITY_FILTER',
   filter
-})
+});
 
 export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id
-})
+});
