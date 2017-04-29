7639eca2daedbe745309b8cdfdbdfe9848a5c8ce
shigemk2 <i.am.shige@gmail.com>
2017-04-29 16:40:18 +0900

    2つのReduceerがありまして、reducersをここでまとめる

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
 const todoApp = combineReducers({
-  // 2つのReduceer
+  // 2つのReduceerがありまして、reducersをここでまとめる
   todos,
   visibilityFilter
 })
