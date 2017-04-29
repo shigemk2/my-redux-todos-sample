2a349eaa83716a9349d254935f7e149d769f1bac
shigemk2 <i.am.shige@gmail.com>
2017-04-29 21:15:44 +0900

    App

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 // ビューの部分。AddTodo/VisibleTodoList/Footerで実装を分離する
-// HTMLのぶぶんは、ここで実装
+// HTMLのぶぶんは、ここで実装する
 const App = () => (
   <div>
     <AddTodo />
