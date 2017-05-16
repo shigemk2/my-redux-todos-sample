c29904d87a866e7a2fd3cfe32a14eea67f88a6ea
shigemk2 <i.am.shige@gmail.com>
2017-05-16 09:05:57 +0900

    todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// todo components
 const Todo = ({ onClick, completed, text }) => (
   <li
     onClick={onClick}
@@ -10,12 +11,12 @@ const Todo = ({ onClick, completed, text }) => (
   >
     {text}
   </li>
-)
+);
 
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
-}
+};
 
 export default Todo
